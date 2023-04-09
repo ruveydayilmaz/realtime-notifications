bec3c7966bebe4f1fc4f6a4b7dcc2c1ada45c717
Ruveyda <ruveyday20@gmail.com>
2023-04-09 18:00:13 +0300

    update: 🖊 socket functions

--- index.js
+++ index.js
@@ -2,20 +2,38 @@ var express = require('express');
 var socket = require('socket.io');
 
 var app = express();
-var server = app.listen(4000, function(){
+var server = app.listen(4000, () => {
     console.log('listening on port 4000');
 });
 
 app.use(express.static('public'));
-
 var io = socket(server);
+
+var activeUsers = [];
 io.on('connection', (socket) => {
+    socket.on("new-user", (userId) => {
+        if (!activeUsers.some((user) => user.userId === userId)) {
+          activeUsers.push({ userId: userId, socketId: socket.id });
+          console.log("User Connected! userId: ", userId);
+          console.log("All Users", activeUsers);
+        }
+    });
 
-    console.log('made connection', socket.id);
-
-    socket.on('notification', function(data){
-        console.log(data);
+    socket.on('notification', (data) => {
+        console.log("Sending to all users...", data);
         socket.broadcast.emit('notification', data);
     });
 
+    socket.on('personal-notification', (data) => {
+        const user = activeUsers.find((user) => user.userId === data.receiver);
+        if(user) {
+          console.log("Sending...", data);
+          io.to(user.socketId).emit("get-personal-notification", data);
+        }
+    });
+
+    socket.on("disconnect", () => {
+        activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+        console.log("User Disconnected! userId: ", socket.id);
+      });
 });
--- public/chat.js
+++ public/chat.js
@@ -1,17 +1,48 @@
 var socket = io.connect('http://localhost:4000', {transports: ['websocket']});
 
 var btn = document.getElementById('send'),
-    output = document.getElementById('output')
+    output = document.getElementById('output'),
+    input = document.getElementById('input'),
+    btn2 = document.getElementById('send2'),
+    input2 = document.getElementById('input2'),
+    user = document.getElementById('user'),
+    output2 = document.getElementById('output2'),
+    chooseDiv = document.getElementById('choose-div'),
+    choose = document.getElementById('choose-btn'),
+    userId = document.getElementById('user-id'),
+    container = document.getElementById('container')
 
-btn.addEventListener('click', function(){
+choose.addEventListener('click', () => {
+    socket.emit('new-user', userId.value);
+    userId.value = "";
+    container.style.display = 'flex';
+    chooseDiv.style.display = 'none';
+});
+
+btn.addEventListener('click', () => {
     socket.emit('notification', {
-        message: "New notification",
+        message: input.value,
         handle: "Admin",
         date: `${new Date().getHours()}:${new Date().getMinutes()}`
     });
-    message.value = "";
+    input.value = "";
 });
 
-socket.on('notification', function(data){
+btn2.addEventListener('click', () => {
+    socket.emit('personal-notification', {
+        message: input2.value,
+        handle: "User",
+        date: `${new Date().getHours()}:${new Date().getMinutes()}`,
+        receiver: user.value
+    });
+    input2.value = "";
+});
+
+socket.on('notification', (data) => {
     output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + ' ' + data.date + '</p>';
+});
+
+socket.on('get-personal-notification', (data) => {
+    console.log('dataa', data)
+    output2.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + ' ' + data.date + '</p>';
 });
